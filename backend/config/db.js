80f97a3f6219a5b4c725ff706973980eb1041c77
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2025-06-27 16:41:57 +0530

    Added MongoDB connection function

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,11 @@
- 
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI_AATT);
+    console.log("MongoDB connected successfully");
+  }catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { connectDB } from './config/db.js';
+
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, ()=>{
+  connectDB();
   console.log(`Listening at: http://localhost:${PORT}`);
 })
