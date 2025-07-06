cf21b10751381995086da53ea8d568129e6bf83b
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2025-07-06 20:26:54 +0530

    Added to business logic of UPI transactions

--- backend/models/account.model.js
+++ backend/models/account.model.js
@@ -32,9 +32,50 @@ const accountSchema = new mongoose.Schema({
   ifsCode: {
     type: String,
     default: 'IFSC0001',
+  },
+  upiId: {
+    type: String,
+    unique: true,
+    sparse: true,
+    match: /^[a-zA-Z0-9.\-_]{2,256}@[a-zA-Z][a-zA-Z0-9.\-_]{2,64}$/
+  },
+  upiPIN: {
+    type: String,
+    select: false,
+    minLength: 4,
+    maxLength: 6
+  },
+  upiEnabled: {
+    type:Boolean,
+    default: false
+  },
+  dailyTransactionLimit: {
+    type: Number,
+    default: 100000
+  },
+  monthlyTransactionLimit: {
+    type: Number,
+    default: 1000000
+  },
+  dailyTransactionCount: {
+    type: Number,
+    default: 0,
+  },
+  monthlyTransactionCount: {
+    type: Number,
+    default: 0
+  },
+  lastTransactionDate: {
+    type: Date
+  },
+  lastMonthlyReset: {
+    type: Date,
+    default: Date.now
   }
 }, {timestamps: true});
 
+accountSchema.index({ userId: 1, accountNo: 1 });
+accountSchema.index({ upiId });
 
 const Account = mongoose.model('Account', accountSchema);
 
